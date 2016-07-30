dc1ba8930761cb6296e0215f5946318af63050a0
Androbin <robin.richtsfeld@gmail.com>
2016-07-30 17:55:26 +0200

    Update App.java

--- src/de/androbin/opengl/App.java
+++ src/de/androbin/opengl/App.java
@@ -1,11 +1,14 @@
 package de.androbin.opengl;
 
 import static org.lwjgl.opengl.GL11.*;
+import java.util.concurrent.atomic.*;
 import org.lwjgl.*;
 import org.lwjgl.opengl.*;
 
 public abstract class App implements Renderable
 {
+	private volatile boolean closeRequested;
+	
 	protected App()
 	{
 		initStandardGL();
@@ -55,11 +58,21 @@ public abstract class App implements Renderable
 		glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
 	}
 	
+	public final boolean isCloseRequested()
+	{
+		return closeRequested;
+	}
+	
+	protected final void requestClose()
+	{
+		closeRequested = true;
+	}
+	
 	public static void run( final App app, final int fps )
 	{
 		long lastFrame = Sys.getTime();
 		
-		while ( !Display.isCloseRequested() )
+		while ( !Display.isCloseRequested() && !app.isCloseRequested() )
 		{
 			if ( Display.wasResized() )
 			{
@@ -67,11 +80,12 @@ public abstract class App implements Renderable
 			}
 			
 			final long thisFrame = Sys.getTime();
-			
-			app.update( (float) ( thisFrame - lastFrame ) / Sys.getTimerResolution() );
-			app.render();
-			
+			final float delta = (float) ( thisFrame - lastFrame ) / Sys.getTimerResolution();
 			lastFrame = thisFrame;
+			
+			app.update( delta );
+			app.updateUI( delta );
+			app.render();
 			
 			Display.update();
 			Display.sync( fps );
@@ -79,8 +93,71 @@ public abstract class App implements Renderable
 		
 		app.destroy();
 		Display.destroy();
-		System.exit( 0 );
+	}
+	
+	public static void runParallel( final App app, final int fps, final long delay )
+	{
+		final AtomicBoolean running = new AtomicBoolean( true );
+		
+		final Thread updater = new Thread( () ->
+		{
+			long lastFrame = Sys.getTime();
+			
+			while ( running.get() )
+			{
+				try
+				{
+					Thread.sleep( delay );
+				}
+				catch ( final InterruptedException ignore )
+				{
+				}
+				
+				final long thisFrame = Sys.getTime();
+				app.update( (float) ( thisFrame - lastFrame ) / Sys.getTimerResolution() );
+				lastFrame = thisFrame;
+			}
+		} );
+		
+		updater.start();
+		
+		long lastFrame = Sys.getTime();
+		
+		while ( running.get() )
+		{
+			if ( Display.wasResized() )
+			{
+				glViewport( 0, 0, Display.getWidth(), Display.getHeight() );
+			}
+			
+			final long thisFrame = Sys.getTime();
+			final float delta = (float) ( thisFrame - lastFrame ) / Sys.getTimerResolution();
+			lastFrame = thisFrame;
+			
+			app.updateUI( delta );
+			app.render();
+			
+			Display.update();
+			Display.sync( fps );
+			
+			running.set( !Display.isCloseRequested() && !app.isCloseRequested() );
+		}
+		
+		try
+		{
+			updater.join();
+		}
+		catch ( final InterruptedException ignore )
+		{
+		}
+		
+		app.destroy();
+		Display.destroy();
 	}
 	
 	protected abstract void update( final float delta );
+	
+	protected void updateUI( final float delta )
+	{
+	}
 }
